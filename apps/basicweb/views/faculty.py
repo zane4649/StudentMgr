a3d0807fd9382a346d6fd718108cc877d27e63ad
root <root@66.42.62.39>
2021-08-22 01:16:24 +0800

    院系数据查询

--- apps/basicweb/views/faculty.py
+++ apps/basicweb/views/faculty.py
@@ -11,18 +11,25 @@ def index(request):
     return render(request, 'basic/faculty.html')
 
 def list_values(request):
+    # 接受查询条件
+    q_str = request.POST.get('queryStr',"")
+
+    # 准备SQL语句
     sql = """
     SELECT T3.Id,T3.Name, Count(*) As 'Number'
     FROM(
         SELECT T1.id, T1.Name 
         FROM Basic_Faculty AS T1
         INNER JOIN Basic_Major AS T2 ON T1.id = T2.faculty_id
+        where T1.Name like '%s'
     ) As T3 
     GROUP BY T3.id,T3.Name
-    """
+    """ % ('%' + q_str + '%')
 
-    response = sqlhelper.get_db_data_dict(sql,['id', 'name', 'number'])
+    # 开始执行
+    response = sqlhelper.get_db_data_dict(sql, ['id', 'name', 'number'])
 
+    # 判断
     if response['status']:
         return JsonResponse({'status':True, 'data':response['data']})
     else:
