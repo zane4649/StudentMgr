c8fa432b11c33ea90e6e473b819c89805acaf50c
root <root@66.42.62.39>
2021-08-28 16:08:46 +0800

    专业课程添加按钮

--- apps/basicweb/views/major.py
+++ apps/basicweb/views/major.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render, HttpResponse
 from apps.basicweb.models import Major
 from django.http import JsonResponse
+from django.db.models import Q
+
 
 def index(request):
     return render(request, 'basic/major.html')
 
 def list_values(request):
     """获取数据"""
-    # 获取所有的数据
-    objs = list(Major.objects.all().values('id', 'name', 'faculty__name'))
+    # 获取传递过来的2个参数
+    page = int(request.POST.get('page', 0))
+    limit = int(request.POST.get('limit', 0))
+    q_str = request.POST.get('inputStr', '')
 
-    # 定义一个返回值类型
-    res = {'code':0, 'data':objs}
+    # 获取所有的数据
+    objs = list(Major.objects.filter(Q(name__icontains=q_str)| Q(faculty__name__icontains=q_str)).values('id', 'name', 'faculty__name'))
+
+    # 获取当前页的数据
+    objs_one_page = objs[(page - 1) * limit: page * limit]
+    # 定义一个返回值类型: code-状态， count-总数，用于分页 data-返回数据
+    res = {'code': 0, 'count': len(objs), 'data': objs_one_page}
     return JsonResponse(res)
+
+
+
+
+
+
+
+
+
+
 
